bd5d25ce2e1eb66c21393c019a4ea7c224af2338
anastasia-kh1 <anastasiakhabuliani@ibsu.edu.ge>
2026-06-23 18:31:50 +0400

    Implement user API endpoints

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDB = require("./config/database");
 
 const movieRoutes = require("./routes/movieRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 connectDB();
 
 app.use("/movies", movieRoutes);
+app.use("/users", userRoutes);
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
